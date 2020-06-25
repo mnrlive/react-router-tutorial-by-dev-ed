26fc6d9c8bf560d5c1b3a520c2495241742b8f14
monmima <monmima@protonmail.com>
2020-06-25 15:48:17 -0400

    tutorial almost finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+import Nav from "./Nav";
+import About from "./About";
+import Shop from "./Shop";
+import Item from "./ItemDetail";
+
+// import a specific part of module with {}
+// rename variable so you don't have to type as much
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+	return (
+		<Router>
+		<div className="App">
+			<Nav />
+
+			{/* Switch only matches one route instead of possibly displaying home page (/) + other route (/about, etc.) */}
+			<Switch>
+				<Route path="/" exact component={Home} />  {/* "exact" means only matches exact route */}
+				<Route path="/about" component={About} />
+				<Route path="/shop" exact component={Shop} />
+				<Route path="/shop/:id" component={Item} />
+			</Switch>
+
+		</div>
+		</Router>
+	);
 }
+
+const Home = () => (
+	<div>
+		<h1>Home Page</h1>
+	</div>
+);
 
 export default App;
--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import {Link} from "react-router-dom";
+
 
 // state
 // the state holds the information that we're getting from the API
@@ -8,33 +8,25 @@ import {Link} from "react-router-dom";
 // useEffect
 // the useEffect basically runs the fetch call when our component mounts
 
-function Shop() {
+function Item() {
 	useEffect(() => {
 		fetchItem();
 	}, []); // "[]" means this is only gonna run when the component mounts
 
-	const [items, setItems] = useState([]);
+	const [item, setItem] = useState({});
 
 	const fetchItem = async () => {
 		const data = await fetch("http://dummy.restapiexample.com/api/v1/employees");
 
-		const items = await data.json();
-		console.log(items.data);
-		setItems(items.data);
+
 	}
 
 	return (
 		<div>
-			<h1>Shop Page</h1>
-			{items.map(item => (
-				<div key={item.id}>
-					<h2 >{item.employee_name}</h2>
+			<h1>Item Detail Page</h1>
 
-					Link to <Link to={`/shop/${item.id}`}>{item.employee_name}</Link>
-				</div>
-			))}
 		</div>
 	);
 }
 
-export default Shop;
+export default Item;
